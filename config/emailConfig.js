cef26def13b17d4a8c7d94a64b1ecf65840db90e
Your Name <you@example.com>
2025-03-06 11:44:26 +0530

    added insights section

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,14 +1,14 @@
 const { Resend } = require('resend');
 require('dotenv').config();
 
-// Initialize Resend with API key
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-// Function to send an email
+
 const sendEmail = async (to, subject, text, html) => {
   try {
     const data = await resend.emails.send({
-      from: process.env.FROM_EMAIL,
+      from: 'Engin <onboarding@resend.dev>',
       to,
       subject,
       text,
@@ -23,4 +23,4 @@ const sendEmail = async (to, subject, text, html) => {
   }
 };
 
-module.exports = { sendEmail };
+module.exports = { sendEmail };                             
